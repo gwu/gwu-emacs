eea7129baf04168344ada6ee111637c2aaf8a63c
Garrett Wu <gwu@blueberry.fios-router.home>
2022-11-15 09:30:24 -0500

    project-specific stuff for typescript

--- init.el
+++ init.el
@@ -132,3 +132,17 @@
   "Visit your emacs init file."
   (interactive)
   (find-file user-init-file))
+
+;;
+;; Specific to my projects.
+;;
+(setq safe-local-variable-values
+      '((eval let
+	      ((project-directory
+		(car
+		 (dir-locals-find-file default-directory))))
+	      (setq lsp-clients-typescript-server-args
+		    `("--tsserver-path"
+		      ,(concat project-directory
+			       ".yarn/sdks/typescript/bin/tsserver")
+		      "--stdio")))))
